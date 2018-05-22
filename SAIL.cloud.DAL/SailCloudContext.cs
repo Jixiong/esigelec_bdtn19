e7f662cf072f52efb8aeaab39019a9cc25cb134a
Pierrick <pierrickblons@gmail.com>
2018-05-22 09:18:35 +0200

    feat: adding Saempty ilCloudContext

---
+++ SAIL.cloud.DAL/SailCloudContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace SAIL.cloud.DAL
+{
+    public class SailCloudContext : DbContext
+    {
+    }
+}
