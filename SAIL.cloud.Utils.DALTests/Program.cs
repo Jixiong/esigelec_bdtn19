e6ca65ce1b459ff7496571dab5e7150eb72c4f2a
Pierrick <pierrickblons@gmail.com>
2018-05-22 09:37:19 +0200

    tests: integration test to validate SailCloudContext against SQLExpress

---
+++ SAIL.cloud.Utils.DALTests/Program.cs
@@ -0,0 +1,21 @@
+using SAIL.cloud.DAL;
+using SAIL.cloud.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAIL.cloud.Utils.DALTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SailCloudContext context = new SailCloudContext();
+            Console.WriteLine(context.Database.Connection.ConnectionString);
+            context.Users.Add(new User() { Id = 1, LastName = "Blons", FirstName = "Pierrick" });
+            context.SaveChanges();
+        }
+    }
+}
